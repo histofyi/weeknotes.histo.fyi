526ae0c81c18a408949c3b6a7c91a16b60e6326f
Chris Thorpe <drchristhorpe@googlemail.com>
2021-08-29 11:17:27 +0100

    WIP

---
+++ content/posts/week_1.md
@@ -0,0 +1,7 @@
+title: Week 1 - hello world, again
+slug: week_1
+category: weeknotes
+date: 2021-07-30
+
+
+# Week 1 - hello world, again
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Christopher Thorpe'
+SITENAME = 'histo.fyi — weeknotes'
+#SITEURL = 'https://weeknotes.histo.fyi'
+SITEURL = ''
+
+PATH = 'content'
+THEME = 'theme'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = ()
+
+# Social widget
+#SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
